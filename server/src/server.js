6314d925499aa9c839a2d8c902dfab9ea9e4ac21
Wr3tchedTorch <ericmoura511@gmail.com>
2026-08-03 15:53:27 -0300

    add initial server configuration

---
+++ server/src/server.js
@@ -0,0 +1,34 @@
+import express from 'express';
+
+const app  = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get("/", (req, res) => {
+  res.status(200).json({status: "success", message: "Welcome to the comic center API."});
+})
+
+const server = app.listen(port, () => {
+  console.log(`Server is now running at http://localhost:${port}.`);
+})
+
+process.on("unhandledRejection", (err) => {
+  console.log("Unhandled Rejection: ", err);
+  server.close(() => {
+    process.exit(1);
+  })
+})
+
+process.on("uncaughtException", (err) => {
+  console.log("Uncaught Exception: ", err);
+  process.exit(1);
+});
+
+process.on("SIGTERM", () => {
+  console.log("SIGTERM received, shutting down gracefully.");
+  server.close(() => {
+    process.exit(0);
+  })
+});
